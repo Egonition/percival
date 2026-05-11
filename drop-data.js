f6bfd8af134835360fc3e16fa9a9763f4907b6af
Egonition <strafe1996@gmail.com>
2026-05-11 13:04:53 -0500

    feat: add Mastery Trials drop category

--- drop-data.js
+++ drop-data.js
@@ -1,55 +1,66 @@
 const DROP_DATA = [
     {
-        category: "Primarch Raids",
+        category: "Primarch",
         drops: [
-        { id: 506, name: "Michael's Anima"},
-        { id: 507, name: "Gabriel's Anima"},
-        { id: 508, name: "Uriel's Anima"},
-        { id: 509, name: "Raphael's Anima"}
+            { id: 506, name: "Michael's Anima"},
+            { id: 507, name: "Gabriel's Anima"},
+            { id: 508, name: "Uriel's Anima"},
+            { id: 509, name: "Raphael's Anima"}
         ]
     },
     {
-        category: "Six-Dragon Raids",
+        category: "Six-Dragon",
         drops: [
-        { id: 557, name: "Wilnas's Jewel"},
-        { id: 558, name: "Wamdus's Jewel"},
-        { id: 559, name: "Galleon's Jewel"},
-        { id: 560, name: "Ewiyar's Jewel"},
-        { id: 561, name: "Lu Woh's Jewel"},
-        { id: 562, name: "Fediel's Jewel"}
+            { id: 557, name: "Wilnas's Jewel"},
+            { id: 558, name: "Wamdus's Jewel"},
+            { id: 559, name: "Galleon's Jewel"},
+            { id: 560, name: "Ewiyar's Jewel"},
+            { id: 561, name: "Lu Woh's Jewel"},
+            { id: 562, name: "Fediel's Jewel"}
+        ]
+    },
+    {
+        category: "Mastery Trials",
+        drops: [
+            { id: 613, name: "Manual of Martial Mastery" },
+            { id: 614, name: "Tome of Martial Secrets"},
+            { id: 615, name: "Manual of Esoteric Mastery" },
+            { id: 616, name: "Tome of Esoteric Secrets"},
+            { id: 617, name: "Manual of Virtuosic Mastery" },
+            { id: 618, name: "Tome of Virtuosic Secrets"},
         ]
     },
     {
         category: "Arcarum",
         drops: [
-        // Astra
-        { id: 25001, name: "Flameborne Astra"},
-        { id: 25002, name: "Aquaborne Astra"},
-        { id: 25003, name: "Earthborne Astra"},
-        { id: 25004, name: "Windborne Astra"},
-        { id: 25005, name: "Lightborne Astra"},
-        { id: 25006, name: "Darkborne Astra"},
-        // Haze
-        { id: 25020, name: "Aurora Haze"},
-        { id: 25021, name: "Chaotic Haze"},
-        // Proof
-        { id: 25023, name: "Fire Verum Proof"},
-        { id: 25024, name: "Water Verum Proof"},
-        { id: 25025, name: "Earth Verum Proof"},
-        { id: 25026, name: "Wind Verum Proof"},
-        // Treasure
-        { id: 25036, name: "Sephira Evolite"},
-        { id: 25074, name: "New World Quartz"}
+            // Astra
+            { id: 25001, name: "Flameborne Astra"},
+            { id: 25002, name: "Aquaborne Astra"},
+            { id: 25003, name: "Earthborne Astra"},
+            { id: 25004, name: "Windborne Astra"},
+            { id: 25005, name: "Lightborne Astra"},
+            { id: 25006, name: "Darkborne Astra"},
+            // Haze
+            { id: 25020, name: "Aurora Haze"},
+            { id: 25021, name: "Chaotic Haze"},
+            // Proof
+            { id: 25023, name: "Fire Verum Proof"},
+            { id: 25024, name: "Water Verum Proof"},
+            { id: 25025, name: "Earth Verum Proof"},
+            { id: 25026, name: "Wind Verum Proof"},
+            // Treasure
+            { id: 25036, name: "Sephira Evolite"},
+            { id: 25074, name: "New World Quartz"}
         ]
     },
     {
         category: "Treasure",
         drops: [
-        { id: 215, name: "Eternity Sand"},
-        { id: 202, name: "Damascus Grain"},
-        { id: 203, name: "Damascus Crystal"},
-        { id: 20004, name: "Gold Brick"},
-        { id: 6513, name: "Terra Adamant"}
+            { id: 215, name: "Eternity Sand"},
+            { id: 202, name: "Damascus Grain"},
+            { id: 203, name: "Damascus Crystal"},
+            { id: 20004, name: "Gold Brick"},
+            { id: 6513, name: "Terra Adamant"}
         ]
     },
   
